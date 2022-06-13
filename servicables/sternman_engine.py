beed22a2549b8c066ba14febb56b8be90559dab2
Zachary Ross <znr3@cornell.edu>
2022-06-13 15:56:22 -0400

    removed bug

--- servicables/sternman_engine.py
+++ servicables/sternman_engine.py
@@ -4,7 +4,6 @@ from engine import Engine
 
 class SternmanEngine(Engine):
     def __init__(self, warning_light_is_on):
-        super().__init__(warning_light_is_on)
         self.warning_light_is_on = warning_light_is_on
 
     def needs_service(self):
